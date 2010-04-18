b36b805d5ddb4284d5fd892d3e49c539b7b528ca
Brock Whitten <brock@sintaxi.com>
2010-04-18 16:14:54 -0700

    create a drinking rule

--- bootstrap.js
+++ bootstrap.js
@@ -4,7 +4,7 @@ function main(){
   // rules to the game
   var statusCodes = [
     [200, "everyone drinks!"],
-    [201, "create a rule. then drink"],
+    [201, "create a drinking rule. then drink"],
     [202, "you will drink, after the next persons turn"],
     [300, "choose multiple people to drink"],
     [301, "choose someone to drink with. its then their turn"],
@@ -17,7 +17,7 @@ function main(){
     [413, "thats what she said! everyone drinks"],
     [417, "last person to put their hand over thir eye drinks"],
     [418, "sing \"I'm a little teapot\" then drink"],
-    [500, "oh fuck, drink."]
+    [500, "oh fuck, drink"]
   ]
   
   // no comment nessisary
@@ -27,5 +27,7 @@ function main(){
   var code = statusCodes[rand][0]
   var head = ["X-Drinking-Instructions", statusCodes[rand][1]]
   var body = "To play the game, view your header (hint: curl http://statuscodedrinkinggame.com -I)\n"
+  
+  // drink
   return [code, head, body];
 }
