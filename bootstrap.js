c806c243a1ee0b4aa807e5c391a1615a1e202931
Rob Ellis <kazoomer@gmail.com>
2010-04-18 12:37:19 -0700

    Added probility and loop to return

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,41 @@
+
+
+function random(minVal,maxVal,floatVal) {
+  var randVal = minVal+(Math.random()*(maxVal-minVal));
+  return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
+}
+
+var prob = [
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    200,200,200,200,200,200,200,200,200,200,
+    500,500,500,500,500,500,500,500,500,500
+];
+
+var code = random(0,prob.length);
+
 function main(){
+  
+  prob[code]
   
   var status_codes = [
     [200, "hello world"],
+    [500, "DRINK"],    
   ];
 
-  return [201, [], "hello there"];
+
+  for (var n = 0; n < status_codes.length;n++) {
+    if (status_codes[n][0] == prob[code]) {
+        return (status_codes[n][1]);
+    }
+  }
+  
+  // return [201, [], "hello there"];
 }
+
