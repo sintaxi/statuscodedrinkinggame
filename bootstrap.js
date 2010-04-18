3b83a1402cffdd4886156a55dc186033cb11bd24
Brock Whitten <brock@sintaxi.com>
2010-04-18 12:40:19 -0700

    added status codes

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,19 @@
 function main(){
   
   var status_codes = [
-    [200, "hello world"],
+    [101, "change direction. everyone drinks!"],
+    [200, "everyone drinks!"],
+    [201, "create a rule. then drink"],
+    [300, "choose several people to drink"],
+    [301, "choose someone to drink with. its then their turn"],
+    [305, "someone else feeds you a drink"],
+    [307, "choose someone to drink"],
+    [401, "everyone but you drinks"],
+    [406, "drink twice"],
+    [410, "remove a rule (if one has been created)"],
+    [411, "take a long drink"],
+    [413, "thats what she said! everyone drinks"],
+    [417, "last person to put their hand over thir eye drinks"]
   ];
 
   return [201, [], "hello there"];
