a9d119754829939ae34da52c7ab2dce3899bdd1f
Rob Ellis <kazoomer@gmail.com>
2010-04-18 13:11:31 -0700

    Added sammy

--- bootstrap.js
+++ bootstrap.js
@@ -5,38 +5,76 @@ function random(minVal,maxVal,floatVal) {
   return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
 }
 
-var prob = [ 101,200,201,300,301,305,307,401,406,410, 411];
+var prob = [101,200,201,300,301,305,307,401,406,410,411,413,417,417,418,500];
 var code = random(0,prob.length -1);
 
-function main(){
-  
-  prob[code]
-  
-  var statusCodes = [
-    [101, "change direction. everyone drinks!"],
-    [200, "everyone drinks!"],
-    [201, "create a rule. then drink"],
-    [300, "choose several people to drink"],
-    [301, "choose someone to drink with. its then their turn"],
-    [305, "someone else feeds you a drink"],
-    [307, "choose someone to drink"],
-    [401, "everyone but you drinks"],
-    [406, "drink twice"],
-    [410, "remove a rule (if one has been created)"],
-    [411, "take a long drink"],
-    [413, "thats what she said! everyone drinks"],
-    [417, "last person to put their hand over thir eye drinks"],
-    [418, "sing I'm a little teapot then drink if people liked it"],
-    [500, "Arrrrrrrrrrrrrr drink."]
-  ];
+system.use('sammy');
 
-  var resp;
+var statusCodes = [
+  [101, "change direction. everyone drinks!"],
+  [200, "everyone drinks!"],
+  [201, "create a rule. then drink"],
+  [300, "choose several people to drink"],
+  [301, "choose someone to drink with. its then their turn"],
+  [305, "someone else feeds you a drink"],
+  [307, "choose someone to drink"],
+  [401, "everyone but you drinks"],
+  [406, "drink twice"],
+  [410, "remove a rule (if one has been created)"],
+  [411, "take a long drink"],
+  [413, "thats what she said! everyone drinks"],
+  [417, "last person to put their hand over thir eye drinks"],
+  [418, "sing I'm a little teapot then drink if people liked it"],
+  [500, "Arrrrrrrrrrrrrr drink."]
+];
+
+
+GET("/",function(){
+  
+  
   for (var n = 0; n < statusCodes.length;n++) {
     if (statusCodes[n][0] == prob[code]) {
         resp = statusCodes[n];
     }
   }
   
-  return [resp[0], [], resp[1] + "\n"];
-}
+  var r = new Response();
+  r.code = resp[0];
+  r.body = resp[1];
+  return r;
+  // return [resp[0], [], resp[1] + "\n"];
+  
+});
+
+// function main(){
+//   
+//   // prob[code]
+//   
+//   var statusCodes = [
+//     [101, "change direction. everyone drinks!"],
+//     [200, "everyone drinks!"],
+//     [201, "create a rule. then drink"],
+//     [300, "choose several people to drink"],
+//     [301, "choose someone to drink with. its then their turn"],
+//     [305, "someone else feeds you a drink"],
+//     [307, "choose someone to drink"],
+//     [401, "everyone but you drinks"],
+//     [406, "drink twice"],
+//     [410, "remove a rule (if one has been created)"],
+//     [411, "take a long drink"],
+//     [413, "thats what she said! everyone drinks"],
+//     [417, "last person to put their hand over thir eye drinks"],
+//     [418, "sing I'm a little teapot then drink if people liked it"],
+//     [500, "Arrrrrrrrrrrrrr drink."]
+//   ];
+// 
+//   var resp;
+//   for (var n = 0; n < statusCodes.length;n++) {
+//     if (statusCodes[n][0] == prob[code]) {
+//         resp = statusCodes[n];
+//     }
+//   }
+//   
+//   return [resp[0], [], resp[1] + "\n"];
+// }
 
